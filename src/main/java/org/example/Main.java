ef6008871240ade5867c1fb50817299a9cc93bbc
Papuk <papukolegvl@gmail.com>
2024-08-01 12:32:36 +0300

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main implements HttpFunction {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
+        BufferedWriter writer = response.getWriter();
+        writer.write("Hello world!");
+    }
+}
